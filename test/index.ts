9afbf2855f8b4eb17e23ebe3f510a1757ecee746
Jim de Beer <beerdejim@gmail.com>
2022-02-15 14:24:00 +0100

    make test

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,24 @@
 import test from 'ava'
 import * as validators from '../src'
 
-test('email', async t => {
+test('validators', async t => {
+  const aNumber = 10
+  t.true(validators.number(aNumber))
+  t.true(validators.float(aNumber))
+  t.true(validators.int(aNumber))
 
-  // yes
+  const aString = 'hello this is a long string hello what is wrong yes'
+  t.false(validators.id(aString))
+  t.true(validators.string(aString))
+  t.false(validators.type(aString))
+
+  const id = 'flap10213312'
+  t.true(validators.id(id))
+  t.true(validators.type(id))
+
+  const url = 'wss://flap.com'
+  t.true(validators.url(url))
+
+  const email = 'jim+hello@saulx.com'
+  t.true(validators.email(email))
 })
